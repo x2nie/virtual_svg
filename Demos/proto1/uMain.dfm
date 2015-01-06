6585f10fe9bd2d7eeff1463cc321b0e9647d336b
x2nie <x2nie@yahoo.com>
2015-01-06 19:54:53 +0700

    several memo for benchmark

--- Demos/proto1/uMain.dfm
+++ Demos/proto1/uMain.dfm
@@ -1,6 +1,6 @@
 object Form1: TForm1
-  Left = 192
-  Top = 80
+  Left = 169
+  Top = 136
   Width = 928
   Height = 480
   Caption = 'Form1'
@@ -15,8 +15,8 @@ object Form1: TForm1
   PixelsPerInch = 96
   TextHeight = 13
   object btn1: TButton
-    Left = 128
-    Top = 48
+    Left = 144
+    Top = 64
     Width = 75
     Height = 25
     Caption = 'btn1'
@@ -24,12 +24,48 @@ object Form1: TForm1
     OnClick = btn1Click
   end
   object mmo1: TMemo
-    Left = 128
+    Left = 40
     Top = 104
     Width = 185
-    Height = 89
+    Height = 249
     Lines.Strings = (
       'mmo1')
     TabOrder = 1
+  end
+  object mmo2: TMemo
+    Left = 280
+    Top = 104
+    Width = 209
+    Height = 249
+    Lines.Strings = (
+      'mmo2')
+    TabOrder = 2
+  end
+  object btn2: TButton
+    Left = 416
+    Top = 64
+    Width = 75
+    Height = 25
+    Caption = 'btn2'
+    TabOrder = 3
+    OnClick = btn2Click
+  end
+  object mmo3: TMemo
+    Left = 520
+    Top = 104
+    Width = 193
+    Height = 249
+    Lines.Strings = (
+      'mmo3')
+    TabOrder = 4
+  end
+  object btn3: TButton
+    Left = 640
+    Top = 64
+    Width = 75
+    Height = 25
+    Caption = 'btn3'
+    TabOrder = 5
+    OnClick = btn3Click
   end
 end
--- Demos/proto1/uMain.pas
+++ Demos/proto1/uMain.pas
@@ -11,8 +11,14 @@ type
   TForm1 = class(TForm)
     btn1: TButton;
     mmo1: TMemo;
+    mmo2: TMemo;
+    btn2: TButton;
+    mmo3: TMemo;
+    btn3: TButton;
     procedure FormCreate(Sender: TObject);
     procedure btn1Click(Sender: TObject);
+    procedure btn2Click(Sender: TObject);
+    procedure btn3Click(Sender: TObject);
   private
     { Private declarations }
     svg : TSVG;
@@ -27,30 +33,84 @@ implementation
 
 {$R *.dfm}
 
+type
+  PSvgVirtualNode = PVirtualNode;
+
 procedure TForm1.FormCreate(Sender: TObject);
 var
   r : ^TDataRectangle;
-  p : PVirtualNode;
+  p : PSvgVirtualNode;
 begin
   svg := TSVG.Create;
-  p := svg.AddChild(nil, SizeOf(TDataRectangle));
-  r := svg.GetData(p);
-  r.x := 100;
-  r.y := 200;
-  r.rx := 300;
-  r.ry := 400;
+
 
 end;
 
 procedure TForm1.btn1Click(Sender: TObject);
 var
   r : ^TDataRectangle;
-  p : PVirtualNode;
+  p : PSvgVirtualNode;
 begin
+  svg.DeleteChildren(svg.RootTag,True);
+  p := svg.AddChild(nil, SizeOf(TDataRectangle));
+
+  r := svg.GetData(p);
+  r.x := 100;
+  r.y := 200;
+  r.rx := 300;
+  r.ry := 400;
+
   p := svg.RootTag.FirstChild;
   r := svg.GetData(p);
 
   mmo1.Lines.Add(Format('x:%f y:%f r:%f,%f',[r.x, r.y, r.rx, r.ry]) );
+
+end;
+
+procedure TForm1.btn2Click(Sender: TObject);
+var
+  r0 : TDataRectangle;
+  r : ^TDataRectangle;
+  p : PSvgVirtualNode;
+begin
+  r0.x := 100;
+  r0.y := 200;
+  r0.rx := 300;
+  r0.ry := 400;
+
+  svg.DeleteChildren(svg.RootTag,True);
+  p := svg.AddChild(nil, SizeOf(TDataRectangle), @r0, True);
+
+
+
+  p := svg.RootTag.FirstChild;
+  r := svg.GetData(p);
+
+  mmo2.Lines.Add(Format('x:%f y:%f r:%f,%f',[r.x, r.y, r.rx, r.ry]) );
+
+end;
+
+procedure TForm1.btn3Click(Sender: TObject);
+var
+  r0 : TDataRectangle;
+  r : ^TDataRectangle;
+  p : PSvgVirtualNode;
+begin
+  r0.x := 100;
+  r0.y := 200;
+  r0.rx := 300;
+  r0.ry := 400;
+
+  svg.DeleteChildren(svg.RootTag,True);
+  //p := svg.AddChild(nil, SizeOf(Tdatarectangle), r0);
+  p := svg.AddChild(nil, SizeOf(r0), r0);
+
+
+
+  p := svg.RootTag.FirstChild;
+  r := svg.GetData(p);
+
+  mmo3.Lines.Add(Format('x:%.0f y:%.0f r:%.0f,%.0f',[r.x, r.y, r.rx, r.ry]) );
 
 end;
 
